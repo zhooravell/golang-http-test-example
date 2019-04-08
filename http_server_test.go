3d0c0443dfd69817344355d4dd6e578576a52e97
Roman Zhuravel <zhuravel.r@centrobill.com>
2019-04-08 09:36:24 +0300

    Added test TestGetRatesOneResourceFail

--- http_server_test.go
+++ http_server_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -13,10 +14,11 @@ import (
 
 type testResource struct {
 	result float64
+	err    error
 }
 
 func (rcv *testResource) BitCoinToUSDRate(ctx context.Context) (float64, error) {
-	return rcv.result, nil
+	return rcv.result, rcv.err
 }
 
 func TestGetRates(t *testing.T) {
@@ -43,6 +45,34 @@ func TestGetRates(t *testing.T) {
 	}
 
 	if string(body) != "BitCoin to USD rate: 15.500000 $\n" {
+		t.Fail()
+	}
+}
+
+func TestGetRatesOneResourceFail(t *testing.T) {
+
+	rateResources = make([]coins_rate.Resource, 2)
+	rateResources[0] = &testResource{result: 10.5}
+	rateResources[1] = &testResource{result: 0, err: errors.New("HTTP FAIL")}
+
+	r, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	w := httptest.NewRecorder()
+
+	getBitcoinRateHandler(w, r)
+
+	resp := w.Result()
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fail()
+	}
+
+	if string(body) != "BitCoin to USD rate: 10.500000 $\n" {
 		t.Fail()
 	}
 }
